a51184c7d6cff3a88bba028e97a3bbc9344b8854
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-04-13 08:40:28 +0200

    Forward unto Dawn!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //This example deals with an array that is being deliberately setup in a way to have its predefined size exceeded with too many write attempts from the for loop.
+        //Usually it throws the ArrayIndexOutOfBoundsException, a pre-configured part of the Java toolkit, and as with all pre-configured exceptions it will terminate the code from the line it is triggered.
+
         int[] array = new int[5];
 
         try {
             for(int i = 0; i < 6; i++) {
                 array[i] = i;
             }
-        } catch(ArrayIndexOutOfBoundsException e) {
+        } catch(ArrayIndexOutOfBoundsException e) { //This is pretty much all the same as the standard pre-defined exception for this type of occurrence. But since we defined it separately it will NOT terminate the program.
             e.printStackTrace();
         }
 
